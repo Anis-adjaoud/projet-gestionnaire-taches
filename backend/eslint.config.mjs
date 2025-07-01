3dac9be7921b43cb1e8d3e50c0bbe30f60464a54
Nicolas Guillot <nicolasguillot92@gmail.com>
2025-07-01 17:02:55 +0200

    change lint conf

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,10 +1,16 @@
 import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+    import globals from "globals";
+    import { defineConfig } from "eslint/config";
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+    export default defineConfig([
+      {
+        files: ["**/*.{js,mjs,cjs}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        rules: {
+          "no-unused-vars": "warn"
+        }
+      },
+      { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+      { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+    ]);
